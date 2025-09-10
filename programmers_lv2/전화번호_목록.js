3359d29f0b350484d17971cba8f0a6b427df94f8
Imhwitae <ghkdgnlxo2@gmail.com>
2025-09-10 21:46:54 +0900

    2025.09.10

---
+++ programmers_lv2/전화번호_목록.js
@@ -0,0 +1,13 @@
+function solution(phone_book) {
+  phone_book.sort(); // 사전순으로 정렬
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(solution(["119", "97674223", "1195524421"]));
